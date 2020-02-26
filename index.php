bf7b62131f0c5d4b0a418503eb0a2355b8c77b91
RoemIko <45184251+RoemIko@users.noreply.github.com>
2020-02-26 21:09:42 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include 'connection.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=”UTF-8″>
+<title>ITSOURCECODE Simple Forum</title>
+</head>
+<body>
+<h3>ITSOURCECODE Simple Forum in PHP/MYSQL</h3>
+<a href=”add-board.php”>Add New Topic Board</a><br>
+<?php
+$sql = "SELECT cat_name, cat_description FROM categories";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["cat_name"]. " Description " . $row["cat_description"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+?>
+</body>
+</html>
+
+
